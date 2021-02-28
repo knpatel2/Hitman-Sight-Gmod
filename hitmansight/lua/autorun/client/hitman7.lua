dba33c0ebb7d63d0165e337d8278a80756fc65cd
Pump3d <76571155+Pump3d@users.noreply.github.com>
2021-02-28 12:47:55 -0500

    Added toggle and gray screen disable option

--- hitmansight/lua/autorun/client/hitman7.lua
+++ hitmansight/lua/autorun/client/hitman7.lua
@@ -1,9 +1,12 @@
 local hitman_sight_bindkey = CreateClientConVar("hitman_sight_bindkey", tostring(KEY_LCONTROL), true, false, "Hitman sight key. Recommended to change in Options > Hitman Sight.")
 --local hitman_sight_slowmode_enabled = CreateClientConVar("hitman_sight_slowmode_enabled", 1, true, false, "Hitman sight slowmode enabled toggle. Recommended to change in Options > Hitman Sight.")
 local hitman_sight_slowmode_intensity = CreateClientConVar("hitman_sight_slowmode_intensity", 0.3, true, false, "Hitman sight slowmode intensity amount. Recommended to change in Options > Hitman Sight.")
+local hitman_sight_toggle = CreateClientConVar("hitman_sight_toggle", 0, true, false, "Hitman sight toggle options. Recommended to change in Options > Hitman Sight.")
+local hitman_sight_gray_screen = CreateClientConVar("hitman_sight_gray_screen", 1, true, false, "Hitman sight gray screen toggle. Recommended to change in Options > Hitman Sight.")
 
 local debounce = false
-local pressed = false
+local pressed = 0
+local key = 0
 
 local settings = {
     [ "$pp_colour_addr" ] = 0,
@@ -16,6 +19,15 @@ local settings = {
     [ "$pp_colour_mulg" ] = 0,
     [ "$pp_colour_mulb" ] = 0
 }
+
+local function checkHook(hooks, name)
+    if hook.GetTable()[hooks][name] == nil then
+        return false
+    else
+        return true
+    end
+end
+
 
 local white = Color(255, 255, 255)
 net.Receive("hitman_sight_friends", function()
@@ -49,9 +61,11 @@ local function onCtrlDown()
         debounce = true
         if GetConVar("hitman_sight_enabled"):GetInt() == 0 then return end
 
-        hook.Add("RenderScreenspaceEffects", "Color", function()
-            DrawColorModify(settings)
-        end)
+        if hitman_sight_gray_screen:GetInt() == 1 then
+            hook.Add("RenderScreenspaceEffects", "hitman_sight_gray_screen", function()
+                DrawColorModify(settings)
+            end)
+        end
         
         net.Start("hitman_sight_clicked")
         net.WriteFloat(hitman_sight_slowmode_intensity:GetFloat(), 4)
@@ -60,7 +74,9 @@ local function onCtrlDown()
 end
 
 local function onCtrlUp()
-    hook.Remove("RenderScreenspaceEffects", "Color")
+    if checkHook("RenderScreenspaceEffects", "hitman_sight_gray_screen") == true then
+        hook.Remove("RenderScreenspaceEffects", "hitman_sight_gray_screen")
+    end
     
     net.Start("hitman_sight_unclicked")
     net.WriteUInt(1, 4)
@@ -73,12 +89,42 @@ local function onCtrlUp()
     debounce = false
 end
 
-hook.Add("Tick", "CheckForCtrl", function()
-    
-    if input.IsKeyDown(hitman_sight_bindkey:GetInt()) == true then
-        onCtrlDown()
-    elseif input.IsKeyDown(hitman_sight_bindkey:GetInt()) == false and debounce == true then
+local function toggle()
+    if pressed == 0 then
+        pressed = 1
+        if GetConVar("hitman_sight_enabled"):GetInt() == 0 then return end
+
+        if hitman_sight_gray_screen:GetInt() == 1 then
+            hook.Add("RenderScreenspaceEffects", "hitman_sight_gray_screen", function()
+                DrawColorModify(settings)
+            end)
+        end
+        
+        net.Start("hitman_sight_clicked")
+        net.WriteFloat(hitman_sight_slowmode_intensity:GetFloat(), 4)
+        net.SendToServer()
+    elseif pressed == 1 then
         onCtrlUp()
+        pressed = 0
+    end
+end
+
+hook.Add("Tick", "CheckForCtrl", function()
+    if input.IsKeyDown(hitman_sight_bindkey:GetInt()) == true then
+        
+        if hitman_sight_toggle:GetInt() == 0 then
+            onCtrlDown()
+        else
+            if key ~= 1 then
+                key = 1
+                toggle()
+            end
+        end
+    elseif input.IsKeyDown(hitman_sight_bindkey:GetInt()) == false then
+        key = 0
+        if hitman_sight_toggle:GetInt() == 0 and debounce == true then
+            onCtrlUp()
+        end        
     end
 end)
 
@@ -89,4 +135,3 @@ net.SendToServer()
 net.Start("hitman_sight_unclicked")
 net.WriteUInt( 1, 4 )
 net.SendToServer()
-
--- hitmansight/lua/autorun/hs_init.lua
+++ hitmansight/lua/autorun/hs_init.lua
@@ -1,5 +1,6 @@
 CreateConVar("hitman_sight_slowmode_enabled", 1)
 CreateConVar("hitman_sight_enabled", 1)
+CreateConVar("hitman_sight_admin_only", 1)
 
 if (CLIENT) then
 	
@@ -11,19 +12,37 @@ if (CLIENT) then
 		spawnmenu.AddToolMenuOption( "Options", "Hitman Sight", "Sight_Options", "#Sight Options", "", "", function(panel)
 			panel:ClearControls()
 
+			panel:AddControl("Label", {
+				Text = "Server"
+			})
+
 			panel:AddControl("Checkbox", {
 				Label = "Enabled",
 				Command = "hitman_sight_enabled"
 			})
 
-			panel:AddControl("Numpad", {
-				Label = "Set the bind key (Default: Left Ctrl)",
-				Command = "hitman_sight_bindkey"
-			})
-
 			panel:AddControl("Checkbox", {
 				Label = "Slowmode enabled",
 				Command = "hitman_sight_slowmode_enabled"
+			})
+
+			panel:AddControl("Label", {
+				Text = "Client"
+			})
+
+			panel:AddControl("Checkbox", {
+				Label = "Toggled",
+				Command = "hitman_sight_toggle"
+			})
+
+			panel:AddControl("Checkbox", {
+				Label = "Gray screen enabled",
+				Command = "hitman_sight_gray_screen"
+			})
+
+			panel:AddControl("Numpad", {
+				Label = "Set the bind key (Default: Left Ctrl)",
+				Command = "hitman_sight_bindkey"
 			})
 			
 			local slider = vgui.Create("DNumSlider", panel)
@@ -38,9 +57,19 @@ if (CLIENT) then
 			--[[panel:AddControl("Checkbox", {
 				Label = "Admin only",
 				Command = "hitman_sight_admin"
-			})]]
-
+			})]]			
 		end)
+	end)
+
+	hook.Add("InitPostEntity", "hitman_sight_load_players", function()
+		if IsValid(LocalPlayer()) and LocalPlayer():IsAdmin() == true then   
+			spawnmenu.AddToolMenuOption( "Options", "Hitman Sight", "Admin_Options", "#Admin Options", "", "", function(panel)
+				panel:AddControl("Checkbox", {
+					Label = "Admin only",
+					Command = "hitman_sight_admin_only"
+				})
+			end)   
+		end
 	end)
 end
 
